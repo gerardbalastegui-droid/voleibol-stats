6098008444af28d0eb15c4cb2be9965a99378c69
gerardbalastegui-droid <gerardbalastegui@gmail.com>
2026-02-01 01:41:08 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Reemplazar $PORT en nginx.conf
-envsubst '$PORT' < /app/nginx.conf > /etc/nginx/nginx.conf
+sed -i "s/\$PORT/$PORT/g" /app/nginx.conf
+cp /app/nginx.conf /etc/nginx/nginx.conf
 
 # Iniciar Streamlit en segundo plano
 streamlit run app.py --server.port=8501 --server.address=127.0.0.1 &
+
+# Esperar un poco a que Streamlit arranque
+sleep 3
 
 # Iniciar nginx en primer plano
 nginx -g 'daemon off;'
